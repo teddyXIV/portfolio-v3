231030732f87d543515c7d646af08895aa295633
Teddy Peterschmidt <t.a.peterschmidt@gmail.com>
2024-03-20 14:50:34 -0700

    removed unnecessary imports

--- src/components/ProjectList.tsx
+++ src/components/ProjectList.tsx
@@ -1,11 +1,6 @@
 import { projectList } from "../data/projectList";
 import Project from './Project';
 import styles from './ProjectList.module.css';
-import 'swiper/css';
-import 'swiper/css/effect-coverflow';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
-import 'swiper/css/scrollbar'
 
 function ProjectList() {
 
