692190818c7e516f063cfb3c35f665792ffe2271
Teddy Peterschmidt <t.a.peterschmidt@gmail.com>
2024-08-20 17:03:17 -0700

    added wip radial graph

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,15 +1,64 @@
 import type { ProjectData } from "../data/projectList"
-import Modal from './Modal';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons'
 import styles from './Project.module.css';
 import { useEffect, useState } from "react";
 import { Octokit } from "octokit";
+import { Radar } from "react-chartjs-2";
+import { 
+    ChartOptions, 
+    ChartData,
+    Chart,
+    RadialLinearScale,
+    PointElement,
+    LineElement,
+    Filler,
+    Tooltip,
+    Legend
+ } from 'chart.js';
+
+ Chart.register(
+    RadialLinearScale,
+    PointElement,
+    LineElement,
+    Filler,
+    Tooltip,
+    Legend
+);
 
 function Project(props: ProjectData) {
     const [expand, setExpand] = useState<boolean>(false)
-    const [languages, setLanguages] = useState<any>(null)
+    const [languages, setLanguages] = useState<string[]>([])
+    const [usage, setUsage] = useState<number[]>([])
+    const [radarData, setRadarData] = useState<ChartData<'radar'>>({
+        labels: [],
+        datasets: [{
+            label: "Languages Used",
+            data: [],
+            backgroundColor: 'rgba(54, 162, 235, 0.2)',
+            borderColor: 'rgba(54, 162, 235, 1)',
+            borderWidth: 1,
+        }]
+    })
+    const options: ChartOptions<'radar'> = {
+        scales: {
+            r: {
+                angleLines: {
+                    display: false
+                },
+                suggestedMin: 50,
+                suggestedMax: 100
+            }
+        }
+    };
 
+    // const options: ChartOptions = {
+    //     elements: {
+    //         line: {
+    //             borderWidth: 3
+    //         }
+    //     }
+    //  }
     
 
     const getLanguages = async () => {
@@ -22,26 +71,41 @@ function Project(props: ProjectData) {
             const response = await octokit.request('GET /repos/teddyxiv/{repo}/languages', {
                 repo: props.id
             })
-            setLanguages(response.data)
+            // setLanguages(response.data)
+            const data = response.data;
+
+            Object.entries(data as { [key: string]: number}).map(([key, value]) => {
+                setLanguages(prevLanguages => [...prevLanguages, key]);
+                setUsage(prevUsage => [...prevUsage, value]);
+            })
+
+            setRadarData(prevState => ({
+                ...prevState,
+                labels: languages,
+                datasets: prevState.datasets.map(dataset => ({
+                    ...dataset,
+                    data: usage
+                }))
+            }))
+
         } catch(error) {
             console.log("Error getting data: ", error);
         }
     }
     
     useEffect(() => {
-        if(expand){
+        if(expand) {
             getLanguages();
         }
     }, [expand])
 
-    const languageList = languages ? Object.entries(languages as { [key: string]: string}).map(([key, value]) => {
+    const languageList = languages.map((lang, index) => {
         return (
-            <li key={key}>
-                {key}: {value}
+            <li key={index}>
+                {lang}
             </li>
         )
-    })
-    : null;
+    });
 
     return (
         <div className={styles.project} key={props.id}>
@@ -56,9 +120,17 @@ function Project(props: ProjectData) {
                         </p>
                         { expand && 
                             <>
-                                <ul>
+                                {/* <ul>
+                                    <li>Just</li>
+                                    <li>Some</li>
+                                    <li>Placeholder</li>
+                                    <li>Points</li>
                                     {languageList}
-                                </ul>
+                                </ul> */}
+                                <LanguageRadar
+                                    data={radarData}
+                                    options={options}/>
+
                                 <a href={props.url} target='_blank' className={styles.githubLink}>
                                     <p>
                                         View on GitHub 
@@ -77,3 +149,14 @@ function Project(props: ProjectData) {
 }
 
 export default Project;
+
+const LanguageRadar: React.FC<{data: ChartData<'radar'>, options: ChartOptions<'radar'>}> = ({ data, options }) => {
+    return (
+        <div>
+            <h2>Language Usage</h2>
+            <Radar 
+                data={data}
+                />
+        </div>
+    )
+}
